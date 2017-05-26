17568a29c4213c5c715685b0bcbaa5eb0a9de1f9
Sean Johnson <sean@snootymonkey.com>
2017-05-26 08:44:21 -0400

    Persist interest in board comments for duration of WS connection in Redis.

--- project.clj
+++ project.clj
@@ -26,6 +26,7 @@
     [open-company/lib "0.10.8-ed2f0f8"] ; Library for OC projects https://github.com/open-company/open-company-lib
     ; In addition to common functions, brings in the following common dependencies used by this project:
     ; httpkit - Web server http://http-kit.org/
+    ; core.async - Async programming and communication https://github.com/clojure/core.async
     ; defun - Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
     ; if-let - More than one binding for if/when macros https://github.com/LockedOn/if-let
     ; Component - Component Lifecycle https://github.com/stuartsierra/component
--- src/oc/interaction/lib/watcher.clj
+++ src/oc/interaction/lib/watcher.clj
@@ -2,12 +2,27 @@
   "Use Redis to track which web socket connections are 'watching' which items."
   (:require [clojure.core.async :as async :refer [<! <!!]]
             [defun.core :refer (defun-)]
+            [taoensso.carmine :as car]
             [taoensso.sente :as sente]
             [taoensso.timbre :as timbre]))
 
 (def watcher-chan (async/chan 10000)) ; buffered channel
 
 (def forever true) ; makes Eastwood happy to have a forever while loop be "conditional" on something
+
+;; ----- Storage constants -----
+
+(def week (* 60 60 24 7)) ; 1 week in seconds for Redis key expiration
+
+;; ----- Redis "Schema" -----
+
+(def watch-prefix "watch:")
+(defn watch-key [watch-id] (str watch-prefix watch-id))
+
+;; ----- Redis connection -----
+
+(def server-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar server-conn ~@body))
 
 ;; ----- Event Handling -----
 
@@ -21,10 +36,22 @@
   "Handle 3 types of messages: watch, unwatch, send"
   
   ([message :guard :watch]
-  (timbre/info "Watch request for" (:watch-id message) "by" (:client-id message)))
+  (let [watch-id (:watch-id message)
+        client-id (:client-id message)
+        redis-key (watch-key watch-id)]
+    (timbre/info "Watch request for" watch-id "by" client-id)
+    (wcar*
+      (car/multi)
+       (car/sadd redis-key client-id)
+        (car/expire redis-key week) ; renew key expiration
+      (car/exec))))
 
   ([message :guard :unwatch]
-  (timbre/info "Stop watch request for" (:watch-id message) "by" (:client-id message)))
+  (let [watch-id (:watch-id message)
+        client-id (:client-id message)]
+    (timbre/info "Stop watch request for" watch-id "by" client-id)
+    (wcar*
+      (car/srem (watch-key watch-id) client-id))))
 
   ([message :guard :send]
   (timbre/info "Send request for" (:watch-id message)))
