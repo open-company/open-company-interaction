4f12cf4acf079faa4a092ef0cfb124ef2bf366a0
bago2k4 <iacopo@rc0.eu>
2020-03-09 22:29:28 +0100

    Use latest lib version.

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
     [clj-http "3.10.0"] ; HTTP client https://github.com/dakrone/clj-http
     [clj-soup/clojure-soup "0.1.3"] ; Clojure wrapper for jsoup HTML parser https://github.com/mfornos/clojure-soup
 
-    [open-company/lib "0.17.25.3-alpha"] ; Library for OC projects https://github.com/open-company/open-company-lib
+    [open-company/lib "0.17.25.3-alpha1"] ; Library for OC projects https://github.com/open-company/open-company-lib
     ;; In addition to common functions, brings in the following common dependencies used by this project:
     ;; httpkit - Web server http://http-kit.org/
     ;; core.async - Async programming and communication https://github.com/clojure/core.async
