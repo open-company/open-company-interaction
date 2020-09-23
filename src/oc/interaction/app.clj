5b7e93bf48751b9ec22f4a17e22cb11f1da686b4
bago2k4 <iacopo@rc0.eu>
2020-09-23 23:52:49 +0200

    Fix sentry ring wrapper.

--- src/oc/interaction/app.clj
+++ src/oc/interaction/app.clj
@@ -53,7 +53,7 @@
 (defn app [sys]
   (cond-> (routes sys)
     ; important that this is first
-    c/dsn             (sentry/wrap sys)
+    c/dsn             (sentry/wrap c/sentry-config)
     c/prod?           wrap-with-logger
     true              wrap-keyword-params
     true              wrap-params
